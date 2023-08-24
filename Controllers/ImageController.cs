ca6978c736bd75e0c3fe500aaa4a75c77bd99fda
Laroub03 <98460971+Laroub03@users.noreply.github.com>
2023-08-24 15:52:45 +0200

    Initial commit

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class ImagesController : ControllerBase
+{
+    [Route("api/images")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadImage()
+        {
+            try
+            {
+                var files = Request.Form.Files;
+
+                if (files == null || files.Count == 0)
+                {
+                    return BadRequest("No files were uploaded.");
+                }
+
+                var base64Images = new List<string>();
+
+                foreach (var file in files)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await file.CopyToAsync(memoryStream);
+                        var bytes = memoryStream.ToArray();
+                        var base64 = Convert.ToBase64String(bytes);
+                        base64Images.Add(base64);
+                    }
+                }
+
+                return Ok(base64Images);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
